36704b6359528f79b29599a48f6bcdfa93242443
Viih-cavalcante <viih.cavalcante@alu.ufc.br>
2025-09-22 01:10:37 -0300

    toalha testando

--- toalhaTest.py
+++ toalhaTest.py
@@ -1,6 +1,16 @@
 class Toalha:
-    def__init__ (self,color str, size str):
-    self.color: str = color
-    self.size: str = size
-    self.wetness: int = 0
+    def __init__(self, color: str, size: str):
+     self.color: str = color
+     self.size: str = size
+     self.wetness: int = 0
 
+minha = Toalha
+self.color = verde
+self.size = media
+self.wetness = 0
+
+
+irma = Toalha
+self.color = vermelho
+self.size =  pequena
+self.wetness = 0
