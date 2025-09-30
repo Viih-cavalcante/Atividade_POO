1652c91faa6c0d2d2243ecd47ce414ff77b1bd47
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-09-30 03:02:23 -0300

    treinando toalha

--- poo/database/enxugar/py/draft.py
+++ poo/database/enxugar/py/draft.py
@@ -1,23 +1,22 @@
-class towel:
-    def __init__(self, color :str, size :str) :
+class towel: 
+    def __ini__(self, color :str, size :str) :
         self.color = color
         self.size = size
         self.Wetness :int = 0 
-
-    def getMaxWetness(self) -> int :
+    
+    def getMaxWetness(self) ->int :
         if self.size == "P" :
             return 10
         if self.size == "M" :
             return 20
-        if self.size == "G":
-            return 30
+        if self.size == "G" :
+            return 30 
         return 0
-    def dry(self, amount :int) -> None :
-        self.Wetness += amount
-        if  self.Wetness > self.getMaxWetness():
-            print("Toalha enxarcada")
-            self.wetness = self.getMaxWetness()
     
+    def dry(self, amount :int) -> None:
+        self.Wetness += amount
+        if self.Wetness > self.getMaxWetness() :
+            print("toalha enxarcada")
+            self.wetness = self.getMaxWetness
 
 
-  
