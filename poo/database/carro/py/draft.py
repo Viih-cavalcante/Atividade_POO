bd13397777467ed20a4b708ad3d69e575d3573f5
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-04 21:43:57 -0300

    tentando fazer o carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,9 +1,20 @@
 class Carro: 
- def __init__(self)
-  self.pass = 0
-  self.km = 0
-  self.passMax = 2
-  self.gas = 0
-  self.gasMax = 100
+    def __init__(self):
+        self.passengers = 0
+        self.passMax = 2
+        self.gas = 0
+        self.km = 0
 
-  def
+    def __str__(self) -> str:
+        return f"pass: {self.passengers}, gas: {self.gas}, km: {self.km}"
+    
+def main ():
+        carro = Carro()
+        while True:
+            line : str = input()
+            print("$" + line)
+            if line == "show":
+                print(carro)
+            elif line == "end":
+                break
+main ()
